12eefe07085c4bf770690d5828356e469b0cd1fe
Milind Mishra <milind.mishra4@gmail.com>
2024-06-23 17:24:48 +0530

    📦 NEW: update favicon and add providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,9 @@
+import { SessionProvider } from "next-auth/react";
+
+type ProviderProps = {
+  children: React.ReactNode;
+};
+
+export default function Providers({ children }: ProviderProps) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
