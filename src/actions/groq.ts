c645dff5634b8a0102237a2e1bb440ccb7f65151
Milind Mishra <milind.mishra4@gmail.com>
2024-06-27 03:14:07 +0530

    🐛 FIX: clean groq action

--- src/actions/groq.ts
+++ src/actions/groq.ts
@@ -1,10 +1,7 @@
 "use server";
 
-import { generateText } from "ai";
-import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
 import { createOpenAI } from "@ai-sdk/openai";
-import { createStreamableValue } from "ai/rsc";
+import { generateText } from "ai";
 import type { Profile } from "~/actions/github";
 import { db } from "~/server/db";
 
@@ -12,19 +9,6 @@ const groq = createOpenAI({
   baseURL: "https://api.groq.com/openai/v1",
   apiKey: process.env.GROQ_API_KEY,
 });
-
-export const streamTextAction = async ({
-  profile,
-}: {
-  profile: Profile | null;
-}) => {
-  const result = await streamText({
-    model: groq("llama3-8b-8192"),
-    temperature: 0.5,
-    prompt: `You are a witty assistant asked to create a profile roast for a GitHub user named ${profile?.login} who has ${profile?.public_repos} repositories and ${profile?.followers} followers.`,
-  });
-  return createStreamableValue(result.textStream).value;
-};
 
 export const findOrGenerateTextAction = async ({
   profile,
